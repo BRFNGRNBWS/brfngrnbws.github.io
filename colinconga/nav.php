c9c0fd9b4d7c77e0cf91b8bc48cd3de0b1923942
BRFNGRNBWS <clnlws44@gmail.com>
2016-10-15 05:28:08 -0700

    uploaded colincon.ga

---
+++ colinconga/nav.php
@@ -0,0 +1,20 @@
+<?php
+echo "<!DOCTYPE html>\n"; 
+echo "<html>\n"; 
+echo "  <head>\n"; 
+echo "    <meta charset=\"UTF-8\">\n"; 
+echo "    <title>usp-s tactical</title>\n"; 
+echo "    <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">\n"; 
+echo "  </head>\n"; 
+echo "  <body>\n"; 
+echo "      <div style=\"width: 100%\">\n"; 
+echo "		<ul style=\"width: 100%;\">\n"; 
+echo "			<li><a href=\"index.html\"><div id=\"nav\"><span id=\"middle\">Home</span></div></a>\n"; 
+echo "			<li><a href=\"art.html\"><div id=\"nav\"><span id=\"middle\">Art</span></div></a></li>\n"; 
+echo "			<li><a href=\"games.html\"><div id=\"nav\"><span id=\"middle\">Games</span></div></a></li>\n"; 
+echo "			<li><a href=\"wish.html\"><div id=\"nav\"><span id=\"middle\">Wish List</span></div></a></li>\n"; 
+echo "		</ul><br><br>\n"; 
+echo "      </div>\n"; 
+echo "  </body>\n"; 
+echo "</html>\n";
+?>
