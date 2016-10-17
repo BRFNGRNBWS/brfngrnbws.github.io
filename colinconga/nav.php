214615c1df033204f4ed287645b0b2e32062ab48
BRFNGRNBWS <clnlws44@gmail.com>
2016-10-17 15:10:25 -0700

    update navbar

--- colinconga/nav.php
+++ colinconga/nav.php
@@ -1,10 +1,8 @@
-<?php
-	echo '<link rel="stylesheet" type="text/css" href="style.css"> -
-	<ul> -
-	<li><a href="index.html"><div id="nav"><span id="middle">Home</span></div></a> -
-	<li><a href="art.html"><div id="nav"><span id="middle">Art</span></div></a></li> -
-	<li><a href="games.html"><div id="nav"><span id="middle">Games</span></div></a></li> -
-	<li><a href="wish.html"><div id="nav"><span id="middle">Wish List</span></div></a></li> -
-	<li><a href="dw.html"><div id="nav"><span id="middle">Doctor Who</span></div></a></li> -
-	</ul>';	
-?>
+echo '<link rel="stylesheet" type="text/css" href="style.css">
+	<ul>
+	<li><a href="index.html"><div id="nav"><span id="middle">Home</span></div></a>
+	<li><a href="art.html"><div id="nav"><span id="middle">Art</span></div></a></li>
+	<li><a href="games.html"><div id="nav"><span id="middle">Games</span></div></a></li>
+	<li><a href="wish.html"><div id="nav"><span id="middle">Wish List</span></div></a></li>
+	<li><a href="dw.html"><div id="nav"><span id="middle">Doctor Who</span></div></a></li>
+	</ul>';
