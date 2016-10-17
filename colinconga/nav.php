c1be1771bb6f9d32e48e9a2575c19091f6d5e5a7
BRFNGRNBWS <clnlws44@gmail.com>
2016-10-17 15:17:00 -0700

    update navbar

--- colinconga/nav.php
+++ colinconga/nav.php
@@ -1,8 +1,8 @@
-echo '<link rel="stylesheet" type="text/css" href="style.css">
-	<ul>
+<link rel="stylesheet" type="text/css" href="style.css">
+<ul>
 	<li><a href="index.html"><div id="nav"><span id="middle">Home</span></div></a>
-	<li><a href="art.html"><div id="nav"><span id="middle">Art</span></div></a></li>
-	<li><a href="games.html"><div id="nav"><span id="middle">Games</span></div></a></li>
-	<li><a href="wish.html"><div id="nav"><span id="middle">Wish List</span></div></a></li>
-	<li><a href="dw.html"><div id="nav"><span id="middle">Doctor Who</span></div></a></li>
-	</ul>';
+		<li><a href="art.html"><div id="nav"><span id="middle">Art</span></div></a></li>
+		<li><a href="games.html"><div id="nav"><span id="middle">Games</span></div></a></li>
+		<li><a href="wish.html"><div id="nav"><span id="middle">Wish List</span></div></a></li>
+		<li><a href="dw.html"><div id="nav"><span id="middle">Doctor Who</span></div></a></li>
+	</ul>	
