4a8074b2d2f5bd19ab8dac84d53326c17f0c6097
BRFNGRNBWS <clnlws44@gmail.com>
2016-10-20 21:06:28 -0700

    navbar bigger attempt

--- colinconga/nav.php
+++ colinconga/nav.php
@@ -1,8 +1,7 @@
-<link rel="stylesheet" type="text/css" href="style.css">
-<ul>
-	<li><a href="index.php"><div id="nav"><span id="middle">Home</span></div></a>
-		<li><a href="art.php"><div id="nav"><span id="middle">Art</span></div></a></li>
-		<li><a href="games.php"><div id="nav"><span id="middle">Games</span></div></a></li>
-		<li><a href="wish.php"><div id="nav"><span id="middle">Wish List</span></div></a></li>
-		<li><a href="dw.php"><div id="nav"><span id="middle">Doctor Who</span></div></a></li>
+<ul width=100%>
+	<li width=100%><a href="index.php"><div id="nav"><span id="middle">Home</span></div></a>
+		<li width=100%><a href="art.php"><div id="nav"><span id="middle">Art</span></div></a></li>
+		<li width=100%><a href="games.php"><div id="nav"><span id="middle">Games</span></div></a></li>
+		<li width=100%><a href="wish.php"><div id="nav"><span id="middle">Wish List</span></div></a></li>
+		<li width=100%><a href="dw.php"><div id="nav"><span id="middle">Doctor Who</span></div></a></li>
 	</ul>	
