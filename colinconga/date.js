27f6463ab8bf7cb9f9d3f1e3b4fabd09397f11cc
BRFNGRNBWS <clnlws44@gmail.com>
2016-12-08 21:04:24 -0800

    back date color1

--- colinconga/date.js
+++ colinconga/date.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
-setInterval(function(){currentTime("#idTimeField")}, 500);
+  setInterval(function(){currentTime("#idTimeField")}, 500);
 });
-function currentTime() {
-var now = new Date();
-var R = (now.getHours() * 10) ;
-var G = (now.getMinutes() * 4) ;
-var B = (now.getSeconds() * 4);
-document.body.style.backgroundColor = 'rgb(' + [R,G,B].join(',') + ')';
+
+
+function currentTime(field) {
+  var now = new Date();
+  now = (now.getHours() * 10) + ', ' + (now.getMinutes() * 4) + ', ' + (now.getSeconds() * 4);
+  $(field).val(now);
+  $('body').css('background-color', "rgb(" + now + ")");
+
 }
