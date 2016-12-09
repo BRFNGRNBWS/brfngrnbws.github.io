fa3f8958a1148aa48bb6b7809f07902694d7fc45
brfngrnbws <clnlws44@gmail.com>
2016-12-08 20:00:50 -0800

    Attempt #1 at background date

---
+++ colinconga/date.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    setInterval(function(){currentTime("#idTimeField")}, 500);
+});
+function currentTime(field) {
+    var now = new Date();
+    now = (now.getHours() * 10) + ', ' + (now.getMinutes() * 4) + ', ' + (now.getSeconds() * 4);
+    $(field).val(now);
+}
+
+document.style.backgroundColor = "rgb(" + now +  	")";
