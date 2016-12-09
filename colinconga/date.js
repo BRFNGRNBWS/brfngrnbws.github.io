86d8036f42d8807d0ea46cdb9a3db96365e528ee
brfngrnbws <clnlws44@gmail.com>
2016-12-09 06:52:33 -0800

    Update date.js

--- colinconga/date.js
+++ colinconga/date.js
@@ -1,12 +1,8 @@
 $(document).ready(function() {
-  setInterval(currentTime(), 1000);
+	setInterval(currentTime(){
+		var now = new Date();
+		var color = (now.getHours() * 10) + ', ' + (now.getMinutes() * 4) + ', ' + (now.getSeconds() * 4);
+		$(field).val(now);
+		$('body').css('background-color', "rgb(" + color + ")");
+	}, 1000);
 });
-
-
-function currentTime(field) {
-  var now = new Date();
-  var color = (now.getHours() * 10) + ', ' + (now.getMinutes() * 4) + ', ' + (now.getSeconds() * 4);
-  $(field).val(now);
-  $('body').css('background-color', "rgb(" + color + ")");
-
-}
