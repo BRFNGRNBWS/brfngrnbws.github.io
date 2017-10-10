659b07c4ae3344eb7b27a57b3d1662e738c3f21a
BRFNGRNBWS <clnlws44@gmail.com>
2017-10-10 09:38:45 -0700

    navbar update

--- colinconga/nav.js
+++ colinconga/nav.js
@@ -1,8 +1,6 @@
 document.getElementById("navcontain").innerHTML = 
-'<ul style="width:100%; padding: 0px; margin: 0px;">' +
-	'<li id="navls"><a href="index.html"><div id="nav"><span id="middle">Home</span></div></a></li>' +
-	'<li id="navls"><a href="art/index.html"><div id="nav"><span id="middle">Art</span></div></a></li>' +
-	'<li id="navls"><a href="games.html"><div id="nav"><span id="middle">Games</span></div></a></li>' + 
-	'<li id="navls"><a href="wish.html"><div id="nav"><span id="middle">Wish List</span></div></a></li>' +
-	'<li id="navls"><a href="dw.html"><div id="nav"><span id="middle">Doctor Who</span></div></a></li>' +
-'</ul>'
+'<li id="nav"><a href="index.html">Home</a></li>' +
+'<li id="nav"><a href="art/index.html">Art</a></li>' +
+'<li id="nav"><a href="games.html"Games</a></li>' + 
+'<li id="nav"><a href="wish.html">Wish List</a></li>' +
+'<li id="nav"><a href="dw.html">Doctor Who</a></li>'
