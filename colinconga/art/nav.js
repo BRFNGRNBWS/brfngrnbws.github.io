335779cf219d273d4f2cd8f903d6f958b2808ccd
Unknown <clnlws44@gmail.com>
2017-10-16 15:25:04 -0700

    art navbar fix

--- colinconga/art/nav.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("navcontain").innerHTML = 
-'<ul style="width:100%; padding: 0px; margin: 0px;">' +
-	'<li id="navls"><a href="../index.html"><div id="nav"><span id="middle">Home</span></div></a></li>' +
-	'<li id="navls"><a href="index.html"><div id="nav"><span id="middle">Art</span></div></a></li>' +
-	'<li id="navls"><a href="../games.html"><div id="nav"><span id="middle">Games</span></div></a></li>' + 
-	'<li id="navls"><a href="../wish.html"><div id="nav"><span id="middle">Wish List</span></div></a></li>' +
-	'<li id="navls"><a href="../dw.html"><div id="nav"><span id="middle">Doctor Who</span></div></a></li>' +
-'</ul>'
