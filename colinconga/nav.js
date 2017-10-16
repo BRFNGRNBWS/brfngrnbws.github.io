37f92be4cb07eb2562caa0e5faeb4e2c9881a60c
Unknown <clnlws44@gmail.com>
2017-10-15 19:35:27 -0700

    navbar links fix

--- colinconga/nav.js
+++ colinconga/nav.js
@@ -1,6 +1,7 @@
 document.getElementById("navcontain").innerHTML = 
+'<li id="mobilenav">Hello Testing</li>' +
 '<li id="nav"><a href="index.html">Home</a></li>' +
 '<li id="nav"><a href="art/index.html">Art</a></li>' +
-'<li id="nav"><a href="games.html"Games</a></li>' + 
+'<li id="nav"><a href="games.html">Games</a></li>' + 
 '<li id="nav"><a href="wish.html">Wish List</a></li>' +
 '<li id="nav"><a href="dw.html">Doctor Who</a></li>'
