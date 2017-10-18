a06658183c684df25d73cec0747909b9ca7fb848
Unknown <clnlws44@gmail.com>
2017-10-17 17:56:01 -0700

    makeover wip

--- colinconga/nav.js
+++ colinconga/nav.js
@@ -7,12 +7,12 @@ var loc = window.location.pathname;
 var dir = loc.substring(0, loc.lastIndexOf('/'))
 
 var navtxt = 
-'<li id="mobilenav">Hello Testing</li>' +
-'<li id="nav"><a href="index.html">Home</a></li>' +
-'<li id="nav"><a href="art/index.html">Art</a></li>' +
-'<li id="nav"><a href="games.html">Games</a></li>' + 
-'<li id="nav"><a href="wish.html">Wish List</a></li>' +
-'<li id="nav"><a href="dw.html">Doctor Who</a></li>';
+'<li id="navtoggle"></li>' +
+'<a href="index.html"><li id="nav">Home</li></a>' +
+'<a href="art/index.html"><li id="nav">Art</li></a>' +
+'<a href="games.html"><li id="nav">Games</li></a>' + 
+'<a href="wish.html"><li id="nav">Wish List</li></a>' +
+'<a href="dw.html"><li id="nav">Doctor Who</li></a>';
 
 if(dir.indexOf('/art') != -1){
 	navtxt = navtxt.replaceAll('href="', 'href="../');
