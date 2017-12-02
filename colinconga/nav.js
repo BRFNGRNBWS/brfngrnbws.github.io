abad687424db6d47d22c8622bae666ba91a240dd
Unknown <clnlws44@gmail.com>
2017-12-01 16:05:22 -0800

    mobile navbar improvement

--- colinconga/nav.js
+++ colinconga/nav.js
@@ -10,9 +10,9 @@ var art = "";
 var navtxt = 
 '<li id="navtoggle"></li>' +
 '<a href="index.html"><li id="nav">Home</li></a>' +
-'<a href="art/index.html"><li id="nav">Art</li></a>' +
+'<a href="art/index.html"><li id="nav" class="everyother">Art</li></a>' +
 '<a href="games/index.html"><li id="nav">Games</li></a>' + 
-'<a href="wish.html"><li id="nav">Wish List</li></a>' +
+'<a href="wish.html"><li id="nav" class="everyother">Wish List</li></a>' +
 '<a href="dw.html"><li id="nav">Doctor Who</li></a>' + 
 '<li id="nav" class="toggle" onclick="themeIs = !themeIs; doTheme();">Toggle Themes</li>';
 
